6f4c0a2297448626b75383b586754470b1402bf5
Pritam <manoharipritam@gmail.com>
2026-03-21 17:04:11 +0530

    add migrations

--- apps/backend/src/config/swagger.ts
+++ apps/backend/src/config/swagger.ts
@@ -11,7 +11,7 @@ export const swaggerSpec = swaggerJSDoc({
     },
     servers: [
       {
-        url: "http://localhost:5000",
+        url: `http://localhost:${process.env.PORT}`,
         description: "Local development server",
       },
     ],
